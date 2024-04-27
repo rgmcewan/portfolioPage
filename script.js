6e9221b2b4b5b31e2b1c0ccb192a1785367a7813
rgmcewan <rgmcewan@outlook.com>
2024-04-27 23:01:43 +1000

    10. Create & Customize Projects - part 1

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const mainImg = document.querySelector(".main-circle img");
 
 let mX = 0;
 let mY = 0;
-const z = 100
+const z = 100;
 
 const animatedCircles = (e,x,y) => {
     if(x < mX) {
@@ -53,7 +53,49 @@ document.body.addEventListener("mousemove", (e) => {
     animatedCircles(e,x,y);
 });
 
-document.body.addEventListener('mouseleave', () => { // Mouse circle and dot disappear when cursor leaves window
-    mouseCircle.style.opacity = '0'
-    mouseDot.style.opacity = '0'
+document.body.addEventListener("mouseleave", () => { // Mouse circle and dot disappear when cursor leaves window
+    mouseCircle.style.opacity = '0';
+    mouseDot.style.opacity = '0';
 });
+
+// Main Button
+const mainBtns = document.querySelectorAll(".main-btn")
+
+mainBtns.forEach(btn => {
+    let ripple;
+
+btn.addEventListener('mouseenter', (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
+
+    ripple = document.createElement("div");
+    ripple.classList.add("ripple");
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    btn.prepend(ripple);
+});
+
+btn.addEventListener("mouseleave", () => {
+    btn.removeChild(ripple);
+});
+})
+
+
+
+// End of Main Button
+
+// About Me text
+const aboutMeText = document.querySelector(".about-me-text");
+const aboutMeTextContent = 
+    "I am a designer & I create award winning websites with the best user experience, that is all... just contact me. :)";
+
+Array.from(aboutMeTextContent).forEach((char) => {
+    const span = document.createElement("span");
+    span.textContent = char;
+    aboutMeText.appendChild(span);
+
+    span.addEventListener('mouseenter',(e) => {
+        e.target.style.animation = "aboutMeTextAnim 10s infinite"
+    })
+});
+// End of About Me text
