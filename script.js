e202cd3f709bb18cd128f7fba39bebcdc32ec44f
rgmcewan <rgmcewan@outlook.com>
2024-04-27 17:16:33 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// Mouse Circle
+const mouseCircle = document.querySelector(".mouse-circle");
+const mouseDot = document.querySelector(".mouse-dot");
+
+const mouseCircleFn = (x, y) => {
+    mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
+    mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
+};
+// End of Mouse Circle
+
+// Animated Circles
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
+
+let mX = 0;
+let mY = 0;
+const z = 100
+
+const animatedCircles = (e,x,y) => {
+    if(x < mX) {
+        circles.forEach((circle) => {
+            circle.style.left = `${z}px`;
+        });
+        mainImg.style.left = `${z}px`;
+    } else if(x > mX) {
+        circles.forEach((circle) => {
+            circle.style.left = `-${z}px`;
+        });
+        mainImg.style.left = `-${z}px`;
+    }
+
+    if(y < mY) {
+        circles.forEach((circle) => {
+            circle.style.top = `${z}px`;
+        });
+        mainImg.style.top = `${z}px`;
+    } else if (y > mY) {
+        circles.forEach((circle) => {
+            circle.style.top = `-${z}px`;
+        });
+        mainImg.style.top = `-${z}px`;
+    }
+
+    mX = e.clientX;
+    mY = e.clientY
+};
+
+document.body.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    mouseCircleFn(x, y);
+    animatedCircles(e,x,y);
+});
+
+document.body.addEventListener('mouseleave', () => { // Mouse circle and dot disappear when cursor leaves window
+    mouseCircle.style.opacity = '0'
+    mouseDot.style.opacity = '0'
+});
