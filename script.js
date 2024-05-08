4b38b1a83a7b87604436c931f3a24b72585ca628
rgmcewan <rgmcewan@outlook.com>
2024-05-08 22:43:23 +1000

    Create & customise Social Media slideshow

--- script.js
+++ script.js
@@ -200,3 +200,24 @@ document.querySelectorAll(".service-btn").forEach((service) => {
 });
 
 // End ofSection 4
+// Section 5
+const formHeading = document.querySelector('.form-heading');
+const formInputs = document.querySelectorAll('.contact-form-input');
+
+formInputs.forEach((input) => {
+    input.addEventListener('focus', () => {
+        formHeading.style.opacity = "0"
+        setTimeout (() => {
+        formHeading.textContent = `Your ${input.placeholder}`;
+        formHeading.style.opacity = '1'}, 300);
+
+    });
+    input.addEventListener('blur', () => {
+        formHeading.style.opacity = "0"
+        setTimeout (() => {
+        formHeading.textContent = "Let's Talk";
+        formHeading.style.opacity = '1'}, 300);
+
+    });
+});
+// End of Section 5
