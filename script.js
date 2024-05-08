59d4dba9e04dd291d4458c361d8b46f671500536
rgmcewan <rgmcewan@outlook.com>
2024-05-08 22:59:26 +1000

    Create & customise social media slideshow

--- script.js
+++ script.js
@@ -207,16 +207,18 @@ const formInputs = document.querySelectorAll('.contact-form-input');
 formInputs.forEach((input) => {
     input.addEventListener('focus', () => {
         formHeading.style.opacity = "0"
-        setTimeout (() => {
+        setTimeout(() => {
         formHeading.textContent = `Your ${input.placeholder}`;
-        formHeading.style.opacity = '1'}, 300);
+        formHeading.style.opacity = "1";
+    }, 300);
 
     });
     input.addEventListener('blur', () => {
         formHeading.style.opacity = "0"
-        setTimeout (() => {
+        setTimeout(() => {
         formHeading.textContent = "Let's Talk";
-        formHeading.style.opacity = '1'}, 300);
+        formHeading.style.opacity = '1';
+    }, 300);
 
     });
 });
